5ff225ae6c8fe613f919c8956733d8fabd37ba72
Dmitry Shelestov <d.shelestov@belitsoft.com>
2024-07-06 09:12:14 +0300

    add envs and img pipe

---
+++ src/app/helpers/pipe/img-url.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Pipe({
+  name: 'imgUrl',
+  standalone: true,
+})
+export class ImgUrlPipe implements PipeTransform {
+  transform(value: string | null): string | null {
+    if (!value) return null;
+
+    return `${environment.BASE_URL}/${value}`;
+  }
+}
